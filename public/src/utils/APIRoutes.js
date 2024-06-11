d4b4995915fce13b57dade7feb37971c1e4c62ca
zishan chaudhary <zishan8291@gmail.com>
2024-06-12 01:32:08 +0530

    fixed server

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://talkchat-3ue4.onrender.com";
 console.log('something');
 export const loginRoute = `${host}/api/auth/login`;
 console.log("something 1");
